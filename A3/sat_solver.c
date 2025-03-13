3371b2991b1592f2cc9836c95170da6329735e29
Tenghoit Kouch <tenghoitkouch+github@gmail.com>
2025-03-12 23:39:50 -0400

    boilerpate a3

---
+++ A3/sat_solver.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function prototypes (if needed)
+
+int main(int argc, char *argv[]) {
+    // Your code here
+    printf("Hello, World!\n");
+    
+    return 0;
+}
