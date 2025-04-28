39d535825997d333c03726c59c61bfe2440e9a94
Tenghoit Kouch <tenghoitkouch+github@gmail.com>
2025-04-27 22:12:45 -0400

    a5 done

--- A5/bnet.py
+++ A5/bnet.py
@@ -1,4 +1,5 @@
 import sys
+import random
 
 
 class Node:
@@ -6,24 +7,24 @@ class Node:
         self.cpt = cpt
         self.parents = parents
 
-    def get_probability(self, val, assignment):
+    def get_probability(self, val: bool, assignment):
 
         if self.parents is None:
             p_true = self.cpt[()]
         else:
-            parent_values = tuple(assignment[parent] for parent in self.parents)
-            p_true = self.cpt[(parent_values)]
+            key = tuple(assignment[parent] for parent in self.parents)
+            p_true = self.cpt[key]
 
         return p_true if val == True else (1 - p_true) # else is for p_false 
 
 
 
 class BayesNet:
-    def __init__(self):
+    def __init__(self, query=None):
         self.nodes = {
-            'E': Node({(): 0.03}),
-
             'B': Node({(): 0.02}),
+
+            'E': Node({(): 0.03}),
 
             'A': Node({
                 (True, True): 0.97,
@@ -33,75 +34,161 @@ class BayesNet:
             }, ['B', 'E']),
 
             'J': Node({
-                (True): 0.85,
-                (False): 0.07
+                (True,): 0.85,
+                (False,): 0.07
             }, ['A']),
 
             'M': Node({
-                (True): 0.69,
-                (False): 0.02
+                (True,): 0.69,
+                (False,): 0.02
             }, ['A'])
         }
 
+        self.assignment = {}
+        self.given = {}
 
-    def full_joint_distribution(self, assignment, given):
-
-        products = []
-
-        for var in assignment.keys():
-            
-            probability = self.nodes[var].get_probability(assignment[var], assignment)
-
-            products.append(probability)
+        if query is not None: self.read_query(query)
 
 
-        result = 1
-        for probability in products:
-            result *= probability
+    def read_query(self, query):
+        truth = {'t': True, 'f': False}
+        query.pop(0) #get rid of program_name
+
+        if 'given' in query:    
+            given_index = query.index('given')
+            given_query = query[given_index + 1:]
+
+            for str in given_query:
+                self.given[str[0]] = truth[str[1]]
+
+            query = query[:given_index]
+        
+        for str in query:
+            self.assignment[str[0]] = truth[str[1]]
 
 
-        return result
+    def __str__(self):
+        return f'Nodes: {self.nodes}\nAssignment: {self.assignment}\nGiven: {self.given}'
+    
+    def build_full_joint_distribution_table(self):
+        # build table
+        table = {}
+        for i in range(32):
+            binary_num = bin(i)[2:].zfill(len(self.nodes))
+
+            current_key = tuple(True if char == '1' else False for char in binary_num)
+
+            current_assignment = {}
+            for index, node in enumerate(self.nodes.keys()):
+                current_assignment[node] = current_key[index]
+
+            # print(current_assignment)
+
+            probabilty = 1.0
+            for key, val in current_assignment.items():
+                probabilty *= self.nodes[key].get_probability(val, current_assignment)
+            # print(probabilty)
+
+            table[current_key] = probabilty
+
+        return table
 
 
+    def full_joint_distribution(self):
+
+        full_joint_distribution_table = self.build_full_joint_distribution_table()
+
+        true_assignment_probability = 0
+        true_given_probability = 0
+        
+        for key, val in full_joint_distribution_table.items():
+
+            current_assignment = {}
+            for index, node in enumerate(self.nodes.keys()):
+                current_assignment[node] = key[index]
+
+            # print(current_assignment)
+
+            # check if matches given
+            match_given_evidence = all(current_assignment.get(node) == self.given.get(node) for node in self.given)
+            if match_given_evidence:
+                true_given_probability += val
+                
+                # check if matches assignment
+                match_assignment = all(current_assignment.get(node) == self.assignment.get(node) for node in self.assignment)
+                if match_assignment:
+                    true_assignment_probability += val
 
 
-
-def process_query(query: list[str], assignment, given):
-    truth = {'t': True, 'f': False}
-
-    query.pop(0) #get rid of program_name
-
-    if 'given' in query:    
-        given_index = query.index('given')
-        given_query = query[given_index + 1:]
-
-        for str in given_query:
-            given[str[0]] = truth[str[1]]
-
-        query = query[:given_index]
+        # print(f'True %: {true_assignment_probability}\nGiven %: {true_given_probability}')
+        return true_assignment_probability/true_given_probability
 
     
-    for str in query:
-        assignment[str[0]] = truth[str[1]]
+    def generate_sample(self):
+
+        new_assignment = {}
+
+        for key, node in self.nodes.items():
+
+            threshold = node.get_probability(True, new_assignment)
+
+            if random.random() < threshold:
+                new_assignment[key] = True
+            else:
+                new_assignment[key] = False
+
+        return new_assignment
+    
+
+    def rejection_sampling(self, num_iterations):
+
+        true_assignment_count = 0
+        true_given_count = 0
+
+        for iteration in range(num_iterations):
+            # print(f'Iteration {iteration}: Assignment #: {true_assignment_count} | Given #: {true_given_count}')
+            
+            new_sample = self.generate_sample()
+
+            # check if matches given
+            match_given_evidence = all(new_sample.get(node) == self.given.get(node) for node in self.given)
+            if match_given_evidence:
+                true_given_count += 1
+                
+                # check if matches assignment
+                match_assignment = all(new_sample.get(node) == self.assignment.get(node) for node in self.assignment)
+                if match_assignment:
+                    true_assignment_count += 1
+
+        return (true_assignment_count/true_given_count) if true_given_count != 0 else 0
+
+
 
 
 
 def main():
 
-    if len(sys.argv) < 2 or len(sys.argv) > 7:
-        exit('Invalid Usage: bnet.py arg1 [arg2 ... arg6]')
+    # if len(sys.argv) < 2 or len(sys.argv) > 7:
+    #     exit('Invalid Usage: bnet.py arg1 [arg2 ... arg6]')
 
-    print(sys.argv)
+    queries = [
+        ['bnet', 'Bt', 'Af', 'given', 'Mf'], 
+        ['bnet', 'Af', 'Et'],
+        ['bnet', 'Jt', 'Af', 'given', 'Bt', 'Ef'],
+        ['bnet', 'Bt', 'Af', 'Mf', 'Jt', 'Et']
+    ]
 
-    assignment = {}
-    given = {}
-
-    process_query(sys.argv, assignment, given)
-
-    BN = BayesNet()
-    result = BN.full_joint_distribution(assignment, given)
-
-    print(f'Assignment: {assignment} | Given: {given} | Result: {result}')
+    for query in queries:
+        print(query)
+        BN = BayesNet(query)
+        print(f'    FJD: {BN.full_joint_distribution()}')
+        # print(BN.generate_sample())
+        rs_results = []
+        for i in range(10):
+            rs_results.append(BN.rejection_sampling(10000000))
+        rs_avg = sum(rs_results)/len(rs_results)
+        print(f'    RS: {rs_avg}')
+    
 
 if __name__ == '__main__':
     main()
